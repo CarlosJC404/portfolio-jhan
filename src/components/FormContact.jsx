535e2bc6510937ca91ab4dcfd0fc96eb2155611b
JhanCarlos <jhancarloscartagena48@gmail.com>
2023-03-04 16:26:05 -0500

    update

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -8,15 +8,18 @@ export const FormContact = () => {
                 <form action="" className="form">
                     <div className="input-box">
                         <label htmlFor="name">Name</label>
-                        <input type="text" id="name"/>
+                        <input type="text" id="name" autoComplete="off"/>
                     </div>
                     <div className="input-box">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email"/>
+                        <input type="email" id="email" autoComplete="off"/>
                     </div>
                     <div className="input-box">
                         <label htmlFor="description">Description</label>
-                        <textarea name="" id="description" cols="30" rows="10"></textarea>
+                        <textarea name="" id="description" cols="30" rows="10" autoComplete="off"></textarea>
+                    </div>
+                    <div className="input-submit">
+                        <button type="submit">Send</button>
                     </div>
                 </form>
             </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,67 @@
+import "../css/Projects.css";
+
 export const Projects = () => {
     return (
         <section id="projects">
             <h1 className="titles">Projects</h1>
+            <div className="cards-projects">
+                <div className="card-project">
+                    <div className="card-img">
+                        <img src="https://play-lh.googleusercontent.com/grT6XcUroIzDWN8PEArwHb_g_tiNudy-u9ycIpc9zHSqgVya6MNJrRweqfiinvCs89Nn=w240-h480-rw" alt="" />
+                    </div>
+                    <div className="card-text">
+                        <div className="card-description">
+                            <h2>Gallery</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere blanditiis quod quo quasi numquam distinctio commodi omnis, atque enim iste ducimus reiciendis.</p>
+                        </div>
+                        <div className="card-info">
+                            <a href="https://www.google.com/search?q=la+luna&oq=la+luna&aqs=chrome.0.69i59.2830j0j4&sourceid=chrome&ie=UTF-8" target="_blank" rel="noopener noreferrer">More info</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-project">
+                    <div className="card-img">
+                        <img src="https://cdn.pixabay.com/photo/2017/10/10/07/48/hills-2836301__340.jpg" alt="" />
+                    </div>
+                    <div className="card-text">
+                        <div className="card-description">
+                            <h2>Natural</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere blanditiis quod quo quasi numquam distinctio commodi omnis, atque enim iste ducimus reiciendis.</p>
+                        </div>
+                        <div className="card-info">
+                            <a href="https://www.google.com/search?q=la+luna&oq=la+luna&aqs=chrome.0.69i59.2830j0j4&sourceid=chrome&ie=UTF-8" target="_blank" rel="noopener noreferrer">More info</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-project">
+                    <div className="card-img">
+                        <img src="https://4kwallpapers.com/images/walls/thumbs_2t/8759.jpg" alt="" />
+                    </div>
+                    <div className="card-text">
+                        <div className="card-description">
+                            <h2>Planet</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere blanditiis quod quo quasi numquam distinctio commodi omnis, atque enim iste ducimus reiciendis.</p>
+                        </div>
+                        <div className="card-info">
+                            <a href="https://www.google.com/search?q=la+luna&oq=la+luna&aqs=chrome.0.69i59.2830j0j4&sourceid=chrome&ie=UTF-8" target="_blank" rel="noopener noreferrer">More info</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-project">
+                    <div className="card-img">
+                        <img src="https://c4.wallpaperflare.com/wallpaper/586/620/26/cyberpunk-city-future-tokyo-artur-sadlos-future-tokyo-hd-wallpaper-preview.jpg" alt="" />
+                    </div>
+                    <div className="card-text">
+                        <div className="card-description">
+                            <h2>Future</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere blanditiis quod quo quasi numquam distinctio commodi omnis, atque enim iste ducimus reiciendis.</p>
+                        </div>
+                        <div className="card-info">
+                            <a href="https://www.google.com/search?q=la+luna&oq=la+luna&aqs=chrome.0.69i59.2830j0j4&sourceid=chrome&ie=UTF-8" target="_blank" rel="noopener noreferrer">More info</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
